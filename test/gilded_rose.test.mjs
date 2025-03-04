a1a327273a2b9d8c12a50e81ba362afad65154c5
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-04 14:12:28 +0200

    Enhance Gilded Rose tests with additional items and expected outcomes

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -4,14 +4,22 @@ import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
   test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([
+      new Item("foo", 0, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 12, 0),
+    ]);
     const items = gildedRose.updateQuality();
 
-    const expected = ["foo"];
+    const expected = [
+      { name: "foo", sellIn: -1 },
+      { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -1 },
+      { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11 },
+    ];
 
     for (let i = 0; i < items.length; i++) {
-      expect(items[i].name).to.equal(expected[i]);
-      expect(items[i].sellIn).to.equal(-1);
+      expect(items[i].name).to.equal(expected[i].name);
+      expect(items[i].sellIn).to.equal(expected[i].sellIn);
     }
   });
 });
