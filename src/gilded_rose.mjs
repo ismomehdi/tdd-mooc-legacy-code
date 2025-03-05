295d8b73a14853c7536446d220c3474d7c7ab7c9
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-05 15:56:38 +0200

    Simplify

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -14,44 +14,52 @@ export class Shop {
   updateQuality() {
     this.items.forEach((item) => {
       if (item.name === "Aged Brie") {
-        if (item.quality < 50) {
-          item.quality += 1;
-        }
-
-        item.sellIn -= 1;
-        if (item.sellIn < 0 && item.quality < 50) {
-          item.quality += 1;
-        }
-      } else {
-        if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.quality < 50) {
-            item.quality += 1;
-            if (item.sellIn < 11 && item.quality < 50) {
-              item.quality += 1;
-              if (item.sellIn < 6) {
-                item.quality += 1;
-              }
-            }
-          }
-          item.sellIn -= 1;
-          if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.name !== "Sulfuras, Hand of Ragnaros") {
-            if (item.quality > 0) {
-              item.quality -= 1;
-            }
-            item.sellIn = item.sellIn - 1;
-            if (item.sellIn < 0) {
-              if (item.quality > 0) {
-                item.quality -= 1;
-              }
-            }
-          }
-        }
+        this.#updateAgedBrie(item);
+      } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+        this.#updateBackstagePass(item);
+      } else if (item.name !== "Sulfuras, Hand of Ragnaros") {
+        this.#updateRegularItem(item);
       }
     });
     return this.items;
+  }
+
+  #updateRegularItem(item) {
+    if (item.quality > 0) {
+      item.quality -= 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
+        item.quality -= 1;
+      }
+    }
+  }
+
+  #updateBackstagePass(item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality += 1;
+        if (item.sellIn < 6) {
+          item.quality += 1;
+        }
+      }
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      item.quality = item.quality - item.quality;
+    }
+  }
+
+  #updateAgedBrie(item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality += 1;
+    }
   }
 }
