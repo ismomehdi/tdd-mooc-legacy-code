9e470dd667143e1f92a7df4e42204a57361dc088
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-05 15:36:04 +0200

    Clean up code

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -15,37 +15,37 @@ export class Shop {
     this.items.forEach((item) => {
       if (item.name === "Aged Brie") {
         if (item.quality < 50) {
-          item.quality = item.quality + 1;
+          item.quality += 1;
         }
 
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
         if (item.sellIn < 0 && item.quality < 50) {
-          item.quality = item.quality + 1;
+          item.quality += 1;
         }
       } else {
         if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
           if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality += 1;
             if (item.sellIn < 11 && item.quality < 50) {
-              item.quality = item.quality + 1;
+              item.quality += 1;
               if (item.sellIn < 6) {
-                item.quality = item.quality + 1;
+                item.quality += 1;
               }
             }
           }
-          item.sellIn = item.sellIn - 1;
+          item.sellIn -= 1;
           if (item.sellIn < 0) {
             item.quality = item.quality - item.quality;
           }
         } else {
           if (item.name !== "Sulfuras, Hand of Ragnaros") {
             if (item.quality > 0) {
-              item.quality = item.quality - 1;
+              item.quality -= 1;
             }
             item.sellIn = item.sellIn - 1;
             if (item.sellIn < 0) {
               if (item.quality > 0) {
-                item.quality = item.quality - 1;
+                item.quality -= 1;
               }
             }
           }
